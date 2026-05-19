3b3246f4ed6d157db1482d6033a27c5cfcdd2abf
Zhidongli-A <3305162655@qq.com>
2026-05-19 21:04:54 +0800

    fix: 移除本地 Chrome 路径配置，使用 Playwright 内核

--- baidu_image_generator.js
+++ baidu_image_generator.js
@@ -9,7 +9,6 @@ class BaiduImageGenerator {
   async init() {
     this.browser = await chromium.launch({
       headless: true,
-      channel: 'chrome',
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
